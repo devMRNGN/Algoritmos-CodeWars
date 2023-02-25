bebb0c97e11c0c0fa4dad2f5a56e7a6accb4d491
João Marangoni <joao.tadeuzi00@gmail.com>
2023-02-25 14:53:32 -0300

    avarage

--- calculateAverage.js
+++ calculateAverage.js
@@ -1,6 +1,6 @@
 function findAverage(array) {
    sumOfArray = array.reduce((acc, cur) => acc+cur, 0);
-   return sumOfArray/array.length;
-}
+   return sumOfArray ? sumOfArray/array.length : 0;
+ }
 
 console.log(findAverage([1,2,3,4]));
