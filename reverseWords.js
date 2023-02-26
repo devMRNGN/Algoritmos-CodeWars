f773d625320f96d4b264801cde4b395c3e3d33f7
João Marangoni <joao.tadeuzi00@gmail.com>
2023-02-26 16:41:13 -0300

    reverseSTRING

--- reverseWords.js
+++ reverseWords.js
@@ -1,5 +1,3 @@
-function reverseWords(str) {
-   
-}
+const reverseWords = str => String((Array.from(str).reverse()).join("")).split(" ").reverse().join(" ");
 
 console.log(reverseWords('double door oi tudo bem'));
