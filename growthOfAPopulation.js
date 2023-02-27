82277c515a6ea25e783719a392fd5fcb5517afdd
João Marangoni <joao.tadeuzi00@gmail.com>
2023-02-27 11:01:54 -0300

    growthOfAPopulation

--- growthOfAPopulation.js
+++ growthOfAPopulation.js
@@ -2,10 +2,11 @@ const nbYear = (p0, percent, aug, p) => {
    let yearCount = 0;
    let actualPopulation = p0;
    do{
+      actualPopulation = Math.ceil(actualPopulation*(1+(percent/100))+aug);
       yearCount++;
-      actualPopulation = Math.round((actualPopulation*(1+(percent/100)))+aug);
-   }while(actualPopulation <= p);
+      console.log(actualPopulation)
+   }while(actualPopulation < p);
    return yearCount;
 }
 
-console.log(nbYear(1500, 5, 100, 5000));
+console.log(nbYear(1500000, 2.5, 10000, 2000000));
