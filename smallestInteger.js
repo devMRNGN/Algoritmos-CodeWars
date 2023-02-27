094d8b8739c1fa718455341616c2c59f8086a282
João Marangoni <joao.tadeuzi00@gmail.com>
2023-02-26 21:57:44 -0300

    smallest integer

--- smallestInteger.js
+++ smallestInteger.js
@@ -1,5 +1,9 @@
 class SmallestIntegerFinder {
-   findSmallestInt(args) {
-     
+   findSmallestInt = args =>  {
+      let smallestNumber = args[0];
+      args.forEach(num => {if(num <= smallestNumber) smallestNumber = num});
+      return smallestNumber;
    }
 }
+let sif = new SmallestIntegerFinder();
+console.log(sif.findSmallestInt([34, -345, -1, 100]));
