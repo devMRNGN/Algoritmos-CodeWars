156940b7107c5b78b676850a0b112376eab85e78
João Marangoni <joao.tadeuzi00@gmail.com>
2023-03-11 08:39:37 -0300

    countchar

---
+++ countCharacteresInYourString[6KYU].js
@@ -0,0 +1,9 @@
+function count (string) {
+   const count = {};
+   string.split("").forEach(s => {
+      count[s] ? count[s]++ : count[s] = 1;
+   });
+   return count;
+}
+
+console.log(count("aba"));
